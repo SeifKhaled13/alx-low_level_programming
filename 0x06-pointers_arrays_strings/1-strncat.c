264c9afd25cfb69dd564e74dd2e3315256db7050
Seif Khaled <seifk359869@gmail.com>
2023-03-02 08:48:57 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,11 @@
 
 /**
  *_strncpy -> function that copies a string
- * @src: the source of strings
- * @dest: the destination of the string
- * @n: the length of int
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
  * Return: pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
